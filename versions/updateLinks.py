b5a39c33b020089e5e6bdf2de48c746f2997e2f1
Mathis Dröge <admin@commandmc.de>
2021-04-07 20:42:20 +0200

    Add the Windows 10 ISO and Media Creation Tool download links
    This involved quite a bit of restructuring, but I like it better like this.
    The main webdriver now has the native user agent again, which probably
    avoids problems down the road

--- versions/updateLinks.py
+++ versions/updateLinks.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 def nvidiaGPU(driver: webdriver.Firefox):
+    print('Getting Nvidia GPU driver info')
     # Load the driver download page
     driver.get('https://www.nvidia.com/Download/index.aspx')
     # Get the Select elements from the site
@@ -34,39 +35,42 @@ def nvidiaGPU(driver: webdriver.Firefox):
     driver.execute_script('GetDriver()')
 
     # Wait for the new page to load and get the download button
-    try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, 'lnkDwnldBtn'))
-        )
-    except TimeoutException:
-        element = None
-        print('Timeout loading download page, something is up...')
-        pass
+    downloadButton = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'lnkDwnldBtn'))
+    )
 
-    if element is not None:
-        url = element.get_attribute('href').split('&')[0].split('?url=')[1]
-        version = url.split('/')[2]
-        # Get the actual download URL from the button link
-        link = 'https://us.download.nvidia.com' + url
-        with open('nvidiaGPU.txt', 'w') as f:
-            f.write(version)
-            f.write("\n")
-            f.write(link)
-            f.write("\n")
-            f.write(datetime.now().strftime('%d/%m/%Y %H:%M'))
+    # Get the actual download URL from the button link
+    url = downloadButton.get_attribute('href').split('&')[0].split('?url=')[1]
+    version = url.split('/')[2]
+    link = 'https://us.download.nvidia.com' + url
+    print('Got Nvidia GPU driver info! Version: ' + version + ', Link: ' + link)
+    with open('nvidiaGPU.txt', 'w') as f:
+        f.write(version)
+        f.write("\n")
+        f.write(link)
+        f.write("\n")
+        f.write(datetime.now().strftime('%d/%m/%Y %H:%M'))
 
 def amdGPU(driver: webdriver.Firefox):
+    print('Getting AMD GPU driver info')
     driver.get('https://www.amd.com/en/support')
     downloadButton = driver.find_element_by_link_text('DOWNLOAD NOW')
     # TODO: Also get latest version. How does AMD even number driver versions?
+    version = ''
     link = downloadButton.get_attribute('href')
+    print('Got AMD GPU driver info! Version: ' + version + ', Link: ' + link)
     with open('amdGPU.txt', 'w') as f:
+        f.write(version)
         f.write('\n')
         f.write(link)
         f.write('\n')
         f.write(datetime.now().strftime('%d/%m/%Y %H:%M'))
 
 def win10(driver: webdriver.Firefox):
+    print('Getting Windows 10 info')
+    #
+    # Version info
+    #
     driver.get('https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html')
     # Find the latest version based on the "Microsoft recommends" text
     recommededByMSText = driver.find_element_by_id('suggested-build-flyout')
@@ -74,32 +78,82 @@ def win10(driver: webdriver.Firefox):
     parent = recommededByMSText.find_element_by_xpath('..')
     # Get the first table data element, which contains the version text
     version = parent.find_element_by_xpath('*').text
+    print('Got W10 version: ' + version)
     
-    driver.get('https://www.microsoft.com/en-us/software-download/windows10')
-    element = driver.find_element_by_id('windows10-upgrade-now')
-    url = element.get_attribute('href')
+    #
+    # ISO download link
+    #
+    driver.get('https://www.microsoft.com/en-us/software-download/windows10ISO')
+    # Get the product edition select element
+    editionSelect = Select(driver.find_element_by_id('product-edition'))
+    # Selecting the 2nd element here should always give us the newest version
+    editionSelect.select_by_index(1)
+    # Click the Confirm button
+    driver.find_element_by_id('submit-product-edition').click()
+    # Repeat the above process for the language select
+    languageSelect = Select(WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'product-languages'))
+    ))
+    # We're just gonna go with English here, who speaks other languages anyways /s
+    languageSelect.select_by_visible_text('English')
+    driver.find_element_by_id('submit-sku').click()
+    # Get the "64-bit Download" button using CSS selectors
+    # This'll probably break someday
+    downloadButton = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, 'a[href*="x64"]'))
+    )
+    ISOlink = downloadButton.get_attribute('href')
+    print('Got W10 ISO link: ' + ISOlink)
+    
+    
+    # We have to create a new driver with a Win User Agent for the update assistant & media creation tool
+    # If we try to use the current one, MS will just redirect to the ISO download
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36")
+    winDriver = webdriver.Firefox(firefox_profile=profile, options=options)
+    
+    #
+    # Update assistant link
+    #
+    winDriver.get('https://www.microsoft.com/en-us/software-download/windows10')
+    upgradeButton = winDriver.find_element_by_id('windows10-upgrade-now')
     # To get the download link, we sadly have to use Requests as Selenium / the webdriver API does not contain the option to send HEAD requests
-    link = head(url, allow_redirects=True).url
+    UAlink = head(upgradeButton.get_attribute('href'), allow_redirects=True).url
+    print('Got W10 update assistant link: ' + UAlink)
+    
+    #
+    # Media creation tool link
+    #
+    downloadButton = winDriver.find_element_by_id('windows10-downloadtool-now')
+    MCTlink = head(downloadButton.get_attribute('href'), allow_redirects=True).url
+    print('Got W10 media creation tool link: ' + MCTlink)
+    
+    winDriver.close()
+    
     with open('win10.txt', 'w') as f:
         f.write(version)
         f.write('\n')
-        f.write(link)
+        f.write(ISOlink)
+        f.write('\n')
+        f.write(UAlink)
+        f.write('\n')
+        f.write(MCTlink)
         f.write('\n')
         f.write(datetime.now().strftime('%d/%m/%Y %H:%M'))
     
 
 if __name__ == "__main__":
+    print('Script started')
     chdir(path[0])
     options = Options()
     options.headless = True
-    # Overwrite the User Agent to make sites think we're actually on Windows
-    # Some sites (looking at you MS) will automatically redirect to different download links when detecting that you're on Linux
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36")
-    driver = webdriver.Firefox(firefox_profile=profile, options=options)
+    driver = webdriver.Firefox(options=options)
+    print('WebDriver constructed. Getting info now...')
     
     nvidiaGPU(driver)
     amdGPU(driver)
     win10(driver)
     
+    print('All links fetched & info gotten! We\'re done here!')
     driver.close()
+    exit(0)
