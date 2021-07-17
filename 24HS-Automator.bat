c39a4964fc7ec54f46f7c893673773270b1b0d1d
BuildTools <admin@commandmc.de>
2021-07-17 17:05:02 +0200

    Add isoFlash
    This is the big one. Took way too long, but I think it turned out
    quite well.

--- 24HS-Automator.bat
+++ 24HS-Automator.bat
@@ -182,6 +182,248 @@ exit /b 0
 
 
 :isoFlash
+call :selectISO selectedISO noFlash osType
+if %ERRORLEVEL% NEQ 0 (
+	echo There was an error when selecting an ISO file. Errorlevel is %ERRORLEVEL%.
+	echo Press any key to return to the main menu...
+	pause >nul
+	exit /b %ERRORLEVEL%
+)
+REM If we don't have to use the flashing tool, we're done now
+if "%noFlash%" EQU "1" (
+	exit /b 0
+)
+REM Launch up the USB drive selector
+call :selectUSBDrive USBdriveIndex USBdriveName
+if %ERRORLEVEL% NEQ 0 (
+	echo There was an error when selecting a USB drive. Errorlevel is %ERRORLEVEL%.
+	echo Press any key to return to the main menu...
+	pause >nul
+	exit /b %ERRORLEVEL%
+)
+echo.
+echo Selected ISO:             %selectedISO%
+echo Selected USB drive name:  %USBdriveName%
+echo Are you absolutely sure these settings are correct? Choosing the wrong device here will lead to data loss!
+choice /c YN
+if %ERRORLEVEL% EQU 2 (
+	echo Returning to main menu...
+	exit /b 0
+)
+echo Cleaning USB drive NOW. Do not unplug the drive!
+call :cleanUSB %USBdriveIndex% "%USBdriveName%" cleanedDriveLetter
+
+echo Downloading flashing program, please wait...
+if "%osType%" EQU "windows" (
+	curl %rufusVersionInfo% --silent --location --output "%dataStorage%\rufus.txt"
+	call :readLineFromFile "%dataStorage%\unetbootin.txt" 2 rufusDownloadLink
+	curl !rufusDownloadLink! --silent --location --output "%dataStorage%\rufus.exe"
+	set command="%dataStorage%\rufus.exe" -i %selectedISO%
+	echo.
+	echo Important: In the Rufus window that'll pop up, select the
+	echo "Select Me" device at the top and press "Start". Do not change
+	echo any other setting!
+	echo.
+	pause
+) else (
+	curl %balenaCliVersionInfo% --silent --location --output "%dataStorage%\balena_cli.txt"
+	call :readLineFromFile "%dataStorage%\balena_cli.txt" 2 balenaCliDownloadLink
+	curl !balenaCliDownloadLink! --silent --location --output "%dataStorage%\balena-cli.zip"
+	if not exist "%dataStorage%\balena-cli\" mkdir "%dataStorage%\balena-cli"
+	tar -xf "%dataStorage%\balena-cli.zip" -C "%dataStorage%"
+	set command="%dataStorage%\balena-cli\balena.exe" local flash "%selectedISO%" -d \\.\PhysicalDrive%USBdriveIndex% -y
+)
+echo Running %command%
+%command%
+echo Flash complete^^! Press any key to return to the main menu...
+pause >nul
+exit /b 0
+
+
+:selectISO selectedISOvar noFlashVar osTypeVar
+curl %win10versionInfo% --silent --location --output "%dataStorage%\win10.txt"
+call :readLineFromFile "%dataStorage%\win10.txt" 1 latestWindowsVersion
+call :readLineFromFile "%dataStorage%\win10.txt" 2 winISO
+call :readLineFromFile "%dataStorage%\win10.txt" 4 winMediaCreationTool
+curl %manjaroDownloadLink% --silent --location --output "%dataStorage%\manjaro.txt"
+call :readLineFromFile "%dataStorage%\manjaro.txt" 1 manjaroISO
+echo Which ISO do you want to flash?
+echo Auto-Download:
+echo (1) Windows 10 (%latestWindowsVersion%, using ISO)
+echo (2) Hiren's BootCD
+echo (3) Memtest86
+echo (4) Manjaro Linux
+echo (5) Windows 10 (%latestWindowsVersion%, using Media Creation Tool)
+echo Custom:
+echo (6) Select ISO file
+choice /c 123456 /N
+echo.
+if %ERRORLEVEL% EQU 1 (
+	echo Downloading Windows 10 %latestWindowsVersion%. Please wait...
+	REM Check if the ISO already exists. We wouldn't want to download a 6GB file twice
+	if exist "%dataStorage%\win10ISO.iso" (
+		REM Ask the user if they want to use the existing file or download the new one
+		echo The ISO file already exists in temp storage. Do you want to use the existing one?
+		choice /c YN
+		REM If they want to download it again, start the download
+		if !ERRORLEVEL! EQU 2 curl %winISO% --location --output "%dataStorage%\win10ISO.iso"
+	) else (
+		REM If the ISO doesn't exist already, just download it
+		curl %winISO% --location --output "%dataStorage%\win10ISO.iso"
+	)
+	REM Whatever happened up there, the ISO should now be here
+	REM (provided the user didn't cancel the download, but then they're on their own anyways)
+	set %1=%dataStorage%\win10ISO.iso
+	set %3=windows
+) else if %ERRORLEVEL% EQU 2 (
+	REM Do the same thing as above, just for the Hirens ISO
+	REM See above for detailed comments
+	echo Downloading Hiren's BootCD. Please wait...
+	if exist "%dataStorage%\hirensISO.iso" (
+		echo The ISO file already exists in temp storage. Do you want to use the existing one?
+		choice /c YN
+		if !ERRORLEVEL! EQU 2 curl %hirensURL% --location --output "%dataStorage%\hirensISO.iso"
+	) else (
+		curl %hirensURL% --location --output "%dataStorage%\hirensISO.iso"
+	)
+	set %1=%dataStorage%\hirensISO.iso
+	set %3=windows
+) else if %ERRORLEVEL% EQU 3 (
+	if not exist "%dataStorage%\memtest\memtest86-usb.img" (
+		REM Memtest comes in a ZIP file with a tool to flash it onto a drive.
+		REM We'll not use that tool and instead just unzip it and use the IMG file
+		echo Downloading and unzipping Memtest86. Please wait...
+		curl %memtestURL% --location --output "%dataStorage%\memtest.zip"
+		if not exist "%dataStorage%\memtest\" mkdir "%dataStorage%\memtest"
+		tar -xf "%dataStorage%\memtest.zip" -C "%dataStorage%\memtest"
+	)
+	set %1=%dataStorage%\memtest\memtest86-usb.img
+	set %3=linux
+) else if %ERRORLEVEL% EQU 4 (
+	echo Downloading Manjaro ISO. Please wait...
+	REM Since Dan's ISO downloads are on Google Drive,
+	REM we have to use a separate download tool (gdown). This is downloaded here.
+	REM Edit: No longer downloaded just here, a little further below
+	if exist "%dataStorage%\manjaroISO.iso" (
+		echo The ISO file already exists in temp storage. Do you want to use the existing one?
+		choice /c YN
+		if !ERRORLEVEL! EQU 2 (
+			curl %gdownURL% --silent --location --output "%dataStorage%\gdown.exe"
+			"%dataStorage%\gdown.exe" --output "%dataStorage%\manjaroISO.iso" %manjaroISO%
+		)
+	) else (
+		curl %gdownURL% --silent --location --output "%dataStorage%\gdown.exe"
+		"%dataStorage%\gdown.exe" --output "%dataStorage%\manjaroISO.iso" %manjaroISO%
+	)
+	REM TODO: Verify the ISO file using %manjaroChecksumURL% (MD5)
+	set %1=%dataStorage%\manjaroISO.iso
+	set %3=linux
+) else if %ERRORLEVEL% EQU 5 (
+	echo Downloading Media Creation Tool. Please wait...
+	curl %winMediaCreationTool% --location --output "%dataStorage%\MediaCreationTool.exe"
+	"%dataStorage%\MediaCreationTool.exe"
+	REM Indicate that the built-in flashing function does not need to be used here
+	set %2=1
+REM Screw the people that press CTRL+C on choice, you have no power here
+) else (
+	REM Download a program to display a file selection prompt (wouldn't want someone to type in the path manually)
+	curl %openFileBoxURL% --location --silent --output "%dataStorage%\OpenFileBox.exe"
+	REM OpenFileBox prints the selected file path into STDOUT, so the only ways to parse that is to either
+	REM put it into a for loop or file. And since it doesn't set the ERRORLEVEL while in a for loop, we sadly
+	REM have to use a temporary file
+	"%dataStorage%\OpenFileBox.exe" "ISO (*.iso)|*.iso|Floppy (*.img)|*.img" "%USERPROFILE%\Downloads" "Open Disk Image File" > "%dataStorage%\selectedISO.txt"
+	if !ERRORLEVEL! EQU 0 (
+		call :readLineFromFile "%dataStorage%\selectedISO.txt" 1 %1
+		REM Once we got that path out of the file we don't need it anymore
+		del "%dataStorage%\selectedISO.txt"
+	) else (
+		exit /b 1
+	)
+	REM Set the osType based on the name: If it has "Windows" in it,
+	REM it's Windows. If not, it's not
+	REM TODO: Do this properly
+	echo %1 | find /i "windows" > nul
+	if !ERRORLEVEL! EQU 0 (
+		set %3=windows
+	) else (
+		set %3=linux
+	)
+)
+exit /b 0
+
+:selectUSBDrive USBdriveIndex USBdriveName
+REM Prompts the user to select a USB drive and stores the index (diskpart) and name
+echo Please connect your USB drive and press any key...
+pause >nul
+REM Get the number of disks currently installed in the system
+echo list disk > "%dataStorage%\diskpartCommand.txt"
+diskpart /s "%dataStorage%\diskpartCommand.txt" > "%dataStorage%\diskpartListDisk.txt"
+set numOfDisks=0
+for /f "skip=8 usebackq delims=" %%a in ("%dataStorage%\diskpartListDisk.txt") do set /a numOfDisks=!numOfDisks!+1
+REM Since the C drive is always connected, we have to subtract 1 here
+set /a numOfDisks=%numOfDisks%-1
+if %numOfDisks% EQU 0 (
+	echo You only have your system disk available. Please make sure the USB drive is connected and working.
+	exit /b 1
+) else if %numOfDisks% EQU -1 (
+	echo You have no disks connected. How did you manage that?
+	exit /b 2
+) else if %numOfDisks% EQU 1 (
+	echo You have 1 disk connected. Do you want to flash the ISO onto it?
+	echo Disk info:
+	call :getDiskpartDetail 1 devName devLtrs
+	echo Disk name: !devName!
+	echo Disk drive letter^(s^): !devLtrs!
+	choice /c YN
+	if !ERRORLEVEL! EQU 2 (
+		echo Returning to main menu...
+		exit /b 3
+	)
+	set %1=1
+	set %2=!devName!
+	exit /b 0
+) else (
+	echo You have %numOfDisks% disks connected. Which one do you want to flash?
+	for /l %%a in (1, 1, %numOfDisks%) do (
+		call :getDiskpartDetail %%a devName devLtrs
+		echo %%a. !devName!, Letters: !devLtrs!
+	)
+	set /p devIndex=Please enter the index of the device [1-%numOfDisks%] and press Enter: 
+	REM Was the index entered greater than 0 and less than/equal to the number of disks?
+	if !devIndex! GEQ 1 if !devIndex! LEQ %numOfDisks% (
+		call :getDiskpartDetail !devIndex! finalDevName notNeeded
+		set %1=!devIndex!
+		set %2=!finalDevName!
+		exit /b 0
+	)
+	exit /b 4
+)
+exit /b 0
+
+:cleanUSB devIndex devName devLtrStorageVar
+REM Runs diskparts clean command after doing some sanity checks
+REM Stores the drive letter of the cleaned device into devLtrStorageVar
+call :getDiskpartDetail %1 diskNameToCheck unused
+REM If the supplied name and the name we got back arent the same, exit right away
+REM if we wouldn't check this, it could cause data loss when the user unplugs the device
+REM before this finishes (regardless of how reckless that would be)
+REM %2 already contains quotes, so we don't have to add them again here
+if %2 NEQ "%diskNameToCheck%" (
+	echo Supplied drive name "%2" is not equal to "%diskNameToCheck%"! Aborting
+	exit /b 1
+)
+echo sel disk %1 > "%dataStorage%\diskpartCommand.txt"
+echo clean >> "%dataStorage%\diskpartCommand.txt"
+echo convert mbr >> "%dataStorage%\diskpartCommand.txt"
+echo create partition primary >> "%dataStorage%\diskpartCommand.txt"
+REM Assign a drive letter we know (makes later operations easier)
+call :getFreeDriveLetter %3
+echo assign letter=!%3! >> "%dataStorage%\diskpartCommand.txt"
+diskpart /s "%dataStorage%\diskpartCommand.txt" > nul
+REM Turns out you can also select volumes by drive letter in diskpart
+echo sel vol !%3! > "%dataStorage%\diskpartCommand.txt"
+echo format fs=exFAT label="Select Me" quick >> "%dataStorage%\diskpartCommand.txt"
+diskpart /s "%dataStorage%\diskpartCommand.txt" > nul
 exit /b 0
 
 
