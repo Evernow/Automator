d2cc2d291e4ffde76d6d592e700fea48b8745468
Mathis Dröge <mathis.droege@ewe.net>
2021-10-20 20:59:37 +0200

    cmd.py: Hide CMD window completely

--- AutomatorGUI/misc/cmd.py
+++ AutomatorGUI/misc/cmd.py
@@ -2,7 +2,7 @@ import logging
 from subprocess import list2cmdline
 from typing import Union
 
-from win32con import SW_SHOWNORMAL
+from win32con import SW_HIDE
 # noinspection PyUnresolvedReferences
 from win32com.shell.shell import ShellExecuteEx
 # noinspection PyUnresolvedReferences
@@ -12,13 +12,13 @@ from win32com.shell.shellcon import SEE_MASK_NOCLOSEPROCESS
 def silent_run_as_admin(command: str) -> Union[dict, bool]:
     logger = logging.getLogger('UACHelper')
     params = list2cmdline([
-        '/c', 'start', '/min', 'cmd', '/c', command
+        '/c', command
     ])
     logger.debug('Trying to run command as admin: \'cmd {}\''.format(params))
     # noinspection PyBroadException
     try:
         admin_proc = ShellExecuteEx(
-            nShow=SW_SHOWNORMAL,
+            nShow=SW_HIDE,
             fMask=SEE_MASK_NOCLOSEPROCESS,
             lpVerb='runas',
             lpFile='cmd',
